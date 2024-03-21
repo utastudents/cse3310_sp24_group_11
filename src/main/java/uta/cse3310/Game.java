b0aab5e46dd5166a68a4163624fde5923d5f1cfa
kevphong <102940029+kevphong@users.noreply.github.com>
2024-03-21 22:15:08 +0000

    Added comments to Game.java

--- src/main/java/uta/cse3310/Game.java
+++ src/main/java/uta/cse3310/Game.java
@@ -9,26 +9,28 @@ public class Game {
     public Statistics Stats;
 
     public void update(UserEvent U){
-    
+        // Processes user actions and updated game state
     }
 
     public void startGame() {
-
+        // Starts the game with player 1
     }
 
     public boolean checkWinner() {
+        // Checks for winner at the end of the game once all words are found
         return true;  // Placeholder return value
     }
 
     public void uniquePlayerColor() {
-
+        // Assigns unique player colors to each player
     }
 
     public boolean checkValidWord(String word) {
+        // Checks if highlighted word by player is a valid word to be scored from the word bank
         return true; // Placeholder return value
     }
 
     public void displayUsersAndData() {
-        
+        // Displays player usernames/handles and their scores
     }
 }
