d265fe8da76decfbcf16701130b079b1596c56cd
Ben <43509597+Ben-jamn@users.noreply.github.com>
2024-04-10 12:55:47 -0500

    Update PlayerTest.java
    started test cases

--- src/test/java/uta/cse3310/PlayerTest.java
+++ src/test/java/uta/cse3310/PlayerTest.java
@@ -1,1 +1,20 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
+public class PlayerTest extends TestCase{
+  
+  public PlayerTest(String testname){
+    super(testname);
+  }
+  
+  public static Test suite()
+  {
+    return new TestSuite( AppTest.class );
+  }
+  public void testNameCheck(){//tests if the 
+    Player player = new Player();
+    player.setName("username");
+    ArrayList<Player> players = new ArrayList<Player>()
+  }
+}
