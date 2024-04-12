411ea9cb7f3e0f08b5337f0e5c9814d0ff56de66
ben2circe@gmail.com <ben2circe@gmail.com>
2024-04-12 00:02:41 -0500

    updated player.java, still not complete/working

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -1,26 +1,34 @@
 package uta.cse3310;
 
+import java.util.*;
 public class Player{
     String playerName;
     int playerScore;
     int playerID;
+    ArrayList<Player> playerList;
 
-    public boolean verifyUsername(String playerName, ArrayList<Player> playerList){
+    public Player(ArrayList<Player> playerList){
+        this.playerList = playerList;
+    }
+
+    public boolean verifyUsername(String playerName){
 
         for (Player player: playerList) {//loops through every player in the player array and checks if any have the same username
-            if(playerName == player.getUsername()){
+            if(playerName == player.getPlayerName()){
                 return false;
             }
         }
         return true;
     }
 
-    public String getUsername() {
+    public String getPlayerName() {
         return playerName;
     }
 
     public void setUsername(String username) {
-        playerName = username;
+        if(verifyUsername(username)){
+            playerName = username;
+        }
     } 
 }
 
