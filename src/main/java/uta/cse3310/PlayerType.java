7a30e2b335c3f9aafb0bed6d00a1ca081a6347bd
Bibekwagle19 <bibekwagle19@gmail.com>
2024-04-13 00:04:28 -0500

    added Playertype

--- src/main/java/uta/cse3310/PlayerType.java
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -1,7 +1,33 @@
 package uta.cse3310;
 
+import java.util.*;
+
+/**
+ * Enum representing different player types with associated colors.
+ */
 public enum PlayerType {
-    // Colors assigned for players
-    Blue, Red, Yellow, Green, NoPlayer
+    BLUE("Blue"),    
+    RED("Red"),      
+    YELLOW("Yellow"), 
+    GREEN("Green"),  
+    NO_PLAYER("NoPlayer"); 
+
+    private final String color; // Color associated with each player type
+
+    /**
+     * Constructor for PlayerType enum.
+     * @param color The color associated with the player type.
+     */
+    PlayerType(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Get the color associated with the player type.
+     * @return The color string.
+     */
+    public String getColor() {
+        return color;
+    }
 }
  
