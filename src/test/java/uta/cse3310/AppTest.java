492b08172c40b4fda32fa9ea8910af6f5a88c918
Jallae16 <122944052+Jallae16@users.noreply.github.com>
2024-04-13 23:38:06 -0500

    Update AppTest.java

--- src/test/java/uta/cse3310/AppTest.java
+++ src/test/java/uta/cse3310/AppTest.java
@@ -1,27 +1,27 @@
-// import junit.framework.TestCase;
-// import java.net.InetSocketAddress;
-// import org.java_websocket.WebSocketImpl;
+import junit.framework.TestCase;
+import java.net.InetSocketAddress;
+import org.java_websocket.WebSocketImpl;
 
-// public class AppTest extends TestCase {
+public class AppTest extends TestCase {
 
-//     /**
-//      * Test creating an App instance with a specific port.
-//      */
-//     public void testAppCreationWithPort() {
-//         int testPort = 9111;
-//         App testApp = new App(testPort);
-//         assertNotNull("App instance should be created", testApp);
-//         InetSocketAddress address = testApp.getAddress();
-//         assertEquals("Port should match the one provided", testPort, address.getPort());
-//     }
+    /**
+     * Test creating an App instance with a specific port.
+     */
+    public void testAppCreationWithPort() {
+        int testPort = 9111;
+        App testApp = new App(testPort);
+        assertNotNull("App instance should be created", testApp);
+        InetSocketAddress address = testApp.getAddress();
+        assertEquals("Port should match the one provided", testPort, address.getPort());
+    }
 
-//     /**
-//      * Test setting reuseAddr flag.
-//      */
-//     public void testSetReuseAddr() {
-//         App testApp = new App(9111);
-//         assertFalse("reuseAddr should initially be false", WebSocketImpl.getReuseAddr());
-//         testApp.setReuseAddr(true);
-//         assertTrue("reuseAddr should be set to true", WebSocketImpl.getReuseAddr());
-//     }
-// }
+    /**
+     * Test setting reuseAddr flag.
+     */
+    public void testSetReuseAddr() {
+        App testApp = new App(9111);
+        assertFalse("reuseAddr should initially be false", WebSocketImpl.getReuseAddr());
+        testApp.setReuseAddr(true);
+        assertTrue("reuseAddr should be set to true", WebSocketImpl.getReuseAddr());
+    }
+}
