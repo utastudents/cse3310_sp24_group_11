fbccffa75a773a6872f8ab537e17c4c26257f4fb
bibekwagle19 <155338453+bibekwagle19@users.noreply.github.com>
2024-04-15 22:50:24 -0500

    Update WordPosition.java

--- src/main/java/uta/cse3310/WordPosition.java
+++ src/main/java/uta/cse3310/WordPosition.java
@@ -2,22 +2,34 @@ package uta.cse3310;
 
 public class WordPosition{
   String word;
-  int startPosition;
-  int endPosition;
-  public WordPosition(String word, int startPosition, int endPosition){
-    this.word = word;
-    this.startPosition = startPosition;
-    this.endPosition = endPosition;
+  int startRow;
+  int startCol;
+  int endRow;
+  int endCol;
+  
+  public WordPosition(String word, int startRow, int startCol, int endRow, int endCol) {
+      this.word = word;
+      this.startRow = startRow;
+      this.startCol = startCol;
+      this.endRow = endRow;
+      this.endCol = endCol;
   }
 
   public String getWord(){
     return word;
   }
-  public int getStart(){
-    return startPosition;
+  public int getstartRow(){
+    return startRow;
   }
-  public int getEnd(){
-    return endPosition;
+  public int getstartCol(){
+    return startCol;
+  }
+
+  public int getendRow(){
+    return endRow;
+  }
+  public int getendCol(){
+    return endCol;
   }
   
 }
