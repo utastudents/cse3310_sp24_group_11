97e76aee128c9a81c82d4cc0ac5c5bf6d033bcaf
ben2circe@gmail.com <ben2circe@gmail.com>
2024-04-29 13:00:41 -0500

    autoTests for game.java

--- src/test/java/uta/cse3310/GameTest.java
+++ src/test/java/uta/cse3310/GameTest.java
@@ -1,16 +1,14 @@
 package uta.cse3310;
 
-import java.util.List;
-
 import junit.framework.TestCase;
 
 
 public class GameTest extends TestCase{
-    public void testPlayerTypeButtonArray(){
+    public void testTwoInput(){//R041 R042 R037
         Game g = new Game();
         g.addPlayer(new Player("player1"));
         g.addPlayer(new Player("player2"));
-        g.startGame(1);
+        g.startGame(null);
 
         //creates a new word to test the word checker and playertype array
 
@@ -20,14 +18,9 @@ public class GameTest extends TestCase{
         g.grid.wordsBank.add("testing");
         
         g.update(new UserEvent(0, PlayerType.Blue, 0));
-        System.out.println("button after 1 input");
-        
-        //printButton(0, 40, g.button);
         
         g.update(new UserEvent(0, PlayerType.Blue, testword.length()-1));
-        System.out.println("button after 2 input ");
         
-        printButton(0, 40, g.button);
         
         for(int i = 0; i < testword.length(); i++){//checks that all buttons are correctly set
             assertTrue(g.button[i] == PlayerType.Blue);
@@ -35,6 +28,57 @@ public class GameTest extends TestCase{
         
 
     }
+
+    public void testInvalidWord(){//R030
+        Game g = new Game();
+        g.addPlayer(new Player("player1"));
+        g.addPlayer(new Player("player2"));
+        g.startGame(null);
+
+        g.update(new UserEvent(0, PlayerType.Blue, 0));
+        
+        g.update(new UserEvent(0, PlayerType.Blue, 1));
+
+        assertEquals(g.button[0], null);
+        assertEquals(g.button[1], null);
+
+        
+    }
+
+    public void testOneInput(){//test requirement R008
+        Game g = new Game();
+        g.addPlayer(new Player("Player 1"));
+        g.addPlayer(new Player("Player 2"));
+        g.startGame(null);
+
+        int buttonInput = 5;
+
+        g.update(new UserEvent(0, PlayerType.Blue, buttonInput));
+
+        assertEquals(g.button[buttonInput], PlayerType.Blue);
+    }
+
+
+    public void testPointsAdded(){//R009 an R010
+        Game g = new Game();
+        g.addPlayer(new Player("player1"));
+        g.addPlayer(new Player("player2"));
+        g.startGame(1);
+
+        String testword = "testing";
+        
+        g.wordGrid.locations.add(new WordPosition(testword, 0, 0, 0, testword.length()-1, "Horizontal Right"));
+        g.grid.wordsBank.add("testing");
+        
+        g.update(new UserEvent(0, PlayerType.Blue, 0));
+        
+        g.update(new UserEvent(0, PlayerType.Blue, testword.length()-1));
+       
+        assertTrue(g.playerList.get(0).playerScore == testword.length());
+        
+
+    }
+
     public void printButton(int start, int end, PlayerType[] array){//method for debugging/testing
         for(int i = start; i <= end; i++){
             if(i%20==0){
