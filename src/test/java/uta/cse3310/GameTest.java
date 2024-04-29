a5acad538b846aec313de477db693b3ae20788a1
ben2circe@gmail.com <ben2circe@gmail.com>
2024-04-28 19:20:35 -0500

    fixing game test - still broken

--- src/test/java/uta/cse3310/GameTest.java
+++ src/test/java/uta/cse3310/GameTest.java
@@ -1,44 +1,47 @@
-// package uta.cse3310;
+package uta.cse3310;
 
-// import junit.framework.TestCase;
+import java.util.List;
+
+import junit.framework.TestCase;
 
 
-// public class GameTest extends TestCase{
-//     public void testPlayerTypeButtonArray(){
-//         Game g = new Game();
-//         g.startGame();
+public class GameTest extends TestCase{
+    public void testPlayerTypeButtonArray(){
+        Game g = new Game();
+        g.startGame(1);
 
-//         //creates a new word to test the word checker and playertype array
+        //creates a new word to test the word checker and playertype array
 
-//         String testword = "testing";
-//         g.wordGrid.locations.add(new WordPosition(testword, 0, 0, 0, testword.length()-1, "Horizontal Right"));
-//         g.grid.wordsBank.add("testing");
+        String testword = "testing";
         
-//         g.update(new UserEvent(0, PlayerType.Blue, 0));
-//         System.out.println("button after 1input");
+        g.wordGrid.locations.add(new WordPosition(testword, 0, 0, 0, testword.length()-1, "Horizontal Right"));
+        g.grid.wordsBank.add("testing");
         
-//         //printButton(0, 40, g.button);
+        g.update(new UserEvent(0, PlayerType.Blue, 0));
+        System.out.println("button after 1 input");
         
-//         g.update(new UserEvent(0, PlayerType.Blue, testword.length()-1));
-//         System.out.println("button after 2 input ");
+        //printButton(0, 40, g.button);
         
-//         //printButton(0, 40, g.button);
+        g.update(new UserEvent(0, PlayerType.Blue, testword.length()-1));
+        System.out.println("button after 2 input ");
         
-//         // for(int i = 0; i < testword.length(); i++){//checks that all buttons are correctly set
-//         //     assertTrue(g.button[i] == PlayerType.Blue);
-//         // }
+        printButton(0, 40, g.button);
+        
+        for(int i = 0; i < testword.length(); i++){//checks that all buttons are correctly set
+            assertTrue(g.button[i] == PlayerType.Blue);
+        }
         
 
-//     }
-//     public void printButton(int start, int end, PlayerType[] array){//method for debugging/testing
-//         for(int i = start; i <= end; i++){
-//             if(i%20==0){
-//                 System.out.println("");
-//             }
-//             System.out.print("["+i+"]"+"|"+array[i]+"|");
+    }
+    public void printButton(int start, int end, PlayerType[] array){//method for debugging/testing
+        for(int i = start; i <= end; i++){
+            if(i%20==0){
+                System.out.println("");
+            }
+            System.out.print("["+i+"]"+"|"+array[i]+"|");
             
-//         }
-//         System.out.println("");
-//     }
+        }
+        System.out.println("");
+    }
     
-// }
+}
